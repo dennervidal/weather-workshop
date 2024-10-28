b3302d8c87f25b8b37859200cd5f0d8794a1d8da
Denner <denner.vidal@gmail.com>
2024-10-28 10:36:30 -0400

    refactor: extract days from fn

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,20 @@
 import { format, parse } from "date-fns";
 
+const DIAS = [
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+  "Domingo",
+];
+
 export const getDayNames = (weather: any) => {
   return weather.forecast.forecastday.map((e: any) => {
     const data = new Date(e.date);
     const diaDaSemana = data.getDay();
-    const dias = [
-      "Segunda",
-      "Terça",
-      "Quarta",
-      "Quinta",
-      "Sexta",
-      "Sábado",
-      "Domingo",
-    ];
-    return dias[diaDaSemana];
+    return DIAS[diaDaSemana];
   });
 };
 
